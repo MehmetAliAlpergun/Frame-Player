27909b9c1616b5b4231150a5cde9348fee105748
Mehmet Ali Alpergün <mehmetalialpergun@gmail.com>
2021-03-17 23:29:56 +0300

    Update frame-player.js

--- docs/js/frame-player.js
+++ docs/js/frame-player.js
@@ -1,6 +1,6 @@
 class FramePlayer {
     constructor(el) {
-        this.images = ["/images/0.jpg", "/images/1.jpg", "/images/2.jpg", "/images/3.jpg", "/images/4.jpg", "/images/5.jpg", "/images/6.jpg"];
+        this.images = ["images/0.jpg", "images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg", "images/5.jpg", "images/6.jpg"];
         this.frames = [];
         this.framewidth = 128;
         this.frameheight = 72;
